295bbe0f68bc52f5caed2eac323ca81708501419
Sorin <jordache_sor@yahoo.com>
2021-09-04 12:19:34 +0300

    added checklist state icons

---
+++ Checklists/Models/Converters.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Checklists.Models
+{
+    public class BoolToIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isDone = (bool)value;
+            if (isDone)
+            {
+                return "/resources/checked_icon.png";
+            }
+            else
+            {
+                return "/resources/unchecked_icon.png";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
